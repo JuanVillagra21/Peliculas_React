ad4ae22f0af14187e92724e738edd152fde41224
JuanVillagra <villagrajuan99@gmail.com>
2022-11-21 14:59:38 -0300

    añadido el proyecto peliculas en react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import Row from 'react-bootstrap/Row';
+import Conteiner from 'react-bootstrap/Container';
+import Pelicula from './Componente/Pelicula.js';
+import peliculas from './json/peliculas.json';
+
+function App() {
+  return (
+    <Conteiner>
+      <Row>
+      <h1>HollyWow</h1>
+        {peliculas.map(peli=>
+          <Pelicula
+          img={peli.img}titulo={peli.titulo}synopsis={peli.synopsis}director={peli.director}reparto={peli.reparto}duracion={peli.duracion}></Pelicula>)}
+
+
+      </Row>
+    </Conteiner>
+
+
+  );
+}
+
+export default App;
+
